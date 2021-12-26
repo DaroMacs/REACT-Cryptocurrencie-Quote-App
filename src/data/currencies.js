62a894c7d4e3594c4f602a2b1f952a6e9a98d159
Daro Macs <84887180+DaroMacs@users.noreply.github.com>
2021-12-26 12:47:21 -0800

    adding MXN currency

--- src/data/currencies.js
+++ src/data/currencies.js
@@ -2,7 +2,8 @@ const currencies = [
     {id:'USD', name:'US Dollar'},
     {id:'CAD', name:'Canadian Dollar'},
     {id:'EUR', name:'Euro'},
-    {id:'GBP', name:'UK Pound'}
+    {id:'GBP', name:'UK Pound'},
+    {id:'MXN', name:'MXN Peso'}
 ]
 
 export {
